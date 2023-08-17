60a9cdcb9f949b5ef854d02e070cdac421e20fc7
Maher TB <maher.altabakh@gmail.com>
2023-08-16 21:31:33 -0400

    init commit

---
+++ Controllers/MotivatingSayingController.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MotivatingSaying.Models;
+using Newtonsoft.Json;
+using System.Net.Http.Json;
+using System.Text.Json.Serialization;
+
+namespace MotivatingSaying.Controllers
+{
+    public class MotivatingSayingController : Controller
+    {
+        public IActionResult MotivationQuote()
+        {
+            HttpClient client = new HttpClient();
+            var responseTask = client.GetAsync("https://zenquotes.io/api/random");
+            responseTask.Wait();
+            if(responseTask.IsCompleted)
+            {
+                var result = responseTask.Result;
+                if(result.IsSuccessStatusCode)
+                {
+                    var MessageTask = result.Content.ReadAsStringAsync();
+
+                    List<MyArray> quote = JsonConvert.DeserializeObject<List<MyArray>>(MessageTask.Result);
+
+                    ViewBag.Quote = quote[0].q;
+                    ViewBag.Author = quote[0].a;
+
+                }
+
+            }
+            return View();
+        }
+
+        public IActionResult Joke()
+        {
+            HttpClient client = new HttpClient();
+            var responseTask = client.GetAsync("https://v2.jokeapi.dev/joke/Programming");
+            responseTask.Wait();
+            if (responseTask.IsCompleted)
+            {
+                var result = responseTask.Result;
+                if (result.IsSuccessStatusCode)
+                {
+                    var MessageTask = result.Content.ReadAsStringAsync();
+
+                    var joke = JsonConvert.DeserializeObject<RootJoke>(MessageTask.Result);
+
+                    ViewBag.joke = joke.setup;
+
+                }
+
+            }
+            return View("MotivationQuote");
+        }
+    }
+}
---
+++ Models/Root.cs
@@ -0,0 +1,41 @@
+﻿namespace MotivatingSaying.Models
+{
+    // Root myDeserializedClass = JsonConvert.DeserializeObject<Root>(myJsonResponse);
+    public class Root
+    {
+        public List<MyArray> MyArray { get; set; }
+    }
+
+    public class MyArray
+    {
+        public string q { get; set; }
+        public string a { get; set; }
+        public string h { get; set; }
+    }
+
+    // Root myDeserializedClass = JsonConvert.DeserializeObject<Root>(myJsonResponse);
+    public class Flags
+    {
+        public bool nsfw { get; set; }
+        public bool religious { get; set; }
+        public bool political { get; set; }
+        public bool racist { get; set; }
+        public bool sexist { get; set; }
+        public bool @explicit { get; set; }
+    }
+
+    public class RootJoke
+    {
+        public bool error { get; set; }
+        public string category { get; set; }
+        public string type { get; set; }
+        public string setup { get; set; }
+        public string delivery { get; set; }
+        public Flags flags { get; set; }
+        public bool safe { get; set; }
+        public int id { get; set; }
+        public string lang { get; set; }
+    }
+
+
+}
